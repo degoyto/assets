78409f303a66580aed1f8e75d4efcced469ecbd7
vitorestevam <vitorestevam02@gmail.com>
2020-02-01 05:59:15 -0300

    first commit

---
+++ scripts/ItemBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehaviour : MonoBehaviour
+{
+
+    public int id;
+    public string nome;
+    public string descricao;
+    public Sprite icone;
+    
+    public void pegaItem(){
+        Debug.Log("apagou");
+        Destroy(this.gameObject);
+        
+    }
+}
