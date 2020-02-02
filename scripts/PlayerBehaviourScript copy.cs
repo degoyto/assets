286d35c447b95331e5728cfec0843acd0c2a9d75
degoyto <degoyto@gmail.com>
2020-02-02 14:52:33 -0300

    adicionei animacao

---
+++ scripts/PlayerBehaviourScript copy.cs
@@ -0,0 +1,196 @@
+﻿  
+using UnityEngine;
+using System.Collections;
+using System.Text;
+
+
+
+public class PlayerBehaviourScript : MonoBehaviour
+{
+	public float speed;                //Floating point variable to store the player's movement speed.
+    public float jumpPower;                //jump speed
+
+    float moveHorizontal; 
+    public bool canjump,jumping;
+    private bool gettingItem=false;
+    private bool taComItem=false;
+    public Transform groundCheck;
+    public PlayerState myState = PlayerState.esperando;     // Estado inicial do player
+    public enum PlayerState                                 // Enumerador de estados do player (pode aumentar)
+    { esperando, normal, interagindo, andando, pulando }
+     
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public ItemBehaviour itemOver,item;
+
+    public GameObject GOitem, objeto, objetoOver, clone; 
+
+    public string itemNome;
+    public Animacao animador;
+    Vector3 flipScale;
+    private AudioSource audio;
+    public bool andando;
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
+        GOitem= this.transform.GetChild(0).gameObject;
+        animador=this.GetComponent<Animacao>();
+        flipScale = transform.localScale;
+    }
+
+
+    void Update(){
+        PlayerPermission();
+        if(Input.GetAxis ("Horizontal")!=0.0f){
+                if(!andando){
+                    andando=true;
+                    AudioManager.instance.Play("Andar");
+                }
+            }
+             if(Input.GetAxis ("Horizontal")==0.0f){
+                 if(andando){
+                    andando=false;
+                    AudioManager.instance.Stop("Andar");
+                }
+            }
+
+    }
+    private void PlayerPermission(){
+        switch(myState){
+            case PlayerState.normal:
+                break;
+            case PlayerState.interagindo:
+                break;
+            case PlayerState.pulando:
+                break;
+            case PlayerState.andando:
+                break;
+        }
+    }
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    // Deixar Input no FixedUpdate pode causa input loss
+    void FixedUpdate()
+    {
+
+        //Store the current horizontal input in the float moveHorizontal.
+            //if(jumping)
+                
+
+        //jump
+       
+       //Pose usar o Input.GetAxisRaw("Vertical") > 0
+        if(jumping && Input.GetKey(KeyCode.UpArrow)){
+             rb2d.AddForce((Vector2.up) * jumpPower, ForceMode2D.Impulse);
+             jumping=false;
+             animador.pulando();
+        }
+        
+        
+        
+        // Pode usar o Input.GetButtonDown("Jump")
+        if(gettingItem && Input.GetKeyUp(KeyCode.Space)){
+             
+            
+            if (itemOver.podePegar){
+                item=itemOver;
+                itemNome=item.nome;
+                item.pegaItem();
+                objeto = EGR.instance.itens[item.id-1];
+               
+                gettingItem = false;
+                taComItem = true;
+                animador.seguraItem();
+                GOitem.GetComponent<SpriteRenderer>().sprite= item.icone;
+            }
+            
+            
+            
+            
+            
+        }
+        if(taComItem && Input.GetKeyDown(KeyCode.Space)){
+            animador.soltaItem();
+            
+            
+            item.criaItem(objeto, GOitem , new Vector2(rb2d.velocity.x,rb2d.velocity.y ));
+            taComItem=false;
+            GOitem.GetComponent<SpriteRenderer>().sprite = null;
+        }
+        
+
+        //move horizontalmente
+        moveHorizontal = Input.GetAxis ("Horizontal");
+        Vector2 movement = new Vector2 (moveHorizontal,0f);        
+        rb2d.AddForce (movement * speed*2);
+        
+        if (moveHorizontal==0){
+            animador.parado();
+            
+        }
+        if (moveHorizontal!=0){
+            animador.andando();
+            
+        }
+        if(moveHorizontal>0){
+            flipScale.x  = 1;
+
+        }
+        if(moveHorizontal<0){
+           flipScale.x  = -1;
+            
+
+        }
+        transform.localScale = flipScale;
+
+       if(moveHorizontal == 0 && rb2d.velocity.x>0){
+           rb2d.velocity=new Vector2(rb2d.velocity.x - 0.3f ,rb2d.velocity.y);
+       }
+       if(moveHorizontal == 0 && rb2d.velocity.x<0){
+           rb2d.velocity=new Vector2(rb2d.velocity.x + 0.3f ,rb2d.velocity.y);
+       }
+            
+       
+
+        if(rb2d.velocity.x > 4.5f){
+            rb2d.velocity=new Vector2 (4.5f,rb2d.velocity.y);
+
+        }
+        else if(rb2d.velocity.x < -4.5f){
+            rb2d.velocity=new Vector2 (-4.5f,rb2d.velocity.y);
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+       
+        if(collision.gameObject.CompareTag("TAGchao")){
+            
+            
+            
+            jumping=true;
+            animador.chao();
+        }    
+    }
+
+     private void OnTriggerEnter2D(Collider2D other) {
+        // if(other.gameObject.CompareTag("TAGferramenta") && InteragirButtonPress()){
+        //     Debug.Log("teste");
+        // }
+        if(other.gameObject.CompareTag("TAGitem")){
+            
+            itemOver=other.GetComponent<ItemBehaviour>();
+            objetoOver = other.gameObject;
+            
+            gettingItem = true;
+        }    
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        itemOver=null;
+        gettingItem = false;
+    }
+    public bool InteragirButtonPress(){
+        return Input.GetButtonDown("Fire1");
+    }
+    
+}
